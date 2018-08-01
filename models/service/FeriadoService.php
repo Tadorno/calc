2370f029786202d23030f96b5b75249b33a5e0a3
Tulio Adorno <tulioadorno@gmail.com>
2018-07-31 22:06:45 -0300

    Ajuste de service

--- models/service/FeriadoService.php
+++ models/service/FeriadoService.php
@@ -22,13 +22,6 @@ class FeriadoService extends ServiceTrait{
         return $model = new FeriadoRecord();
     }
     
-    /**
-     * Finds the Exemplo model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return FeriadoRecord the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
     protected function findModel($id)
     {
         if (($model = FeriadoRecord::findOne($id)) !== null) {
